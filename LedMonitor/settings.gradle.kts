c388b04c5205ac2a4536e85b481d21b90fc01fe0
phanmemthptdinhlap <89722266+phanmemthptdinhlap@users.noreply.github.com>
2026-08-26 01:34:48 +0700

    Configure Android project settings in settings.gradle.kts
    Added Android and Kotlin plugins with configuration.

--- LedMonitor/settings.gradle.kts
+++ LedMonitor/settings.gradle.kts
@@ -1,2 +1,16 @@
-rootProject.name = "LedMonitor"
-include(":app")
+plugins {
+    id("com.android.application") version "8.1.4"
+    id("org.jetbrains.kotlin.android") version "1.9.0"
+}
+
+android {
+    namespace = "com.led.monitor"
+    compileSdk = 34
+    defaultConfig {
+        applicationId = "com.led.monitor"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
